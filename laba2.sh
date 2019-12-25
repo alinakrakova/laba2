ca6839dea65fa10ce01d6bdc0687e62149e6c766
alinakrakova <55484211+alinakrakova@users.noreply.github.com>
2019-12-25 11:55:14 +0300

    Update laba2.sh

--- laba2.sh
+++ laba2.sh
@@ -4,9 +4,9 @@ wget -c https://dl.google.com/go/go1.13.1.linux-amd64.tar.gz
 sudo tar -C /usr/local -xvzf go1.13.1.linux-amd64.tar.gz 
 mkdir -p ~/go
 mkdir -p ~/go_projects/{bin,src,pkg} 
-echo 'export PATH=$PATH:/usr/local/go/bin' » /etc/profile
-echo 'export GOPATH="$HOME/go_projects"' » ~/.profile
-echo 'export GOBIN="$GOPATH/bin"' » ~/.profile 
+echo 'export PATH=$PATH:/usr/local/go/bin' >> /etc/profile
+echo 'export GOPATH="$HOME/go_projects"' >> ~/.profile
+echo 'export GOBIN="$GOPATH/bin"' >> ~/.profile 
 source ~/.profile
 git clone https://github.com/yggdrasil-network/yggdrasil-go.git
 cd yggdrasil-go
